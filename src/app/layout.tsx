91eb4058b0155ec9ba95023928ca1efc16ce95ec
ocsoriram <ossoriram@gmail.com>
2025-07-21 02:18:11 +0900

    feat: ヘッダーを作 成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import Header from "./components/HEader";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -29,7 +30,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${notoSansJP.className} antialiased`}>{children}</body>
+      <body className={`${notoSansJP.className} antialiased`}>
+        <Header />
+        {children}
+      </body>
       {/* <body
         className={`${geistMono.variable} ${geistSans.variable} antialiased`}
       >
