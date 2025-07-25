187c4ce9d94affe06b7d8f290b71a51c69fb311e
ocsoriram <ossoriram@gmail.com>
2025-07-25 12:42:43 +0900

    feat: NextAuthのプロバイダー取得の認証用APIを作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,13 @@
+import { getProviders } from "next-auth/react";
+
 async function Login() {
+
+  const providers = await getProviders()
+                    .then((res) => {
+                      // console.log(res);
+                      return res;
+                    });
+
   return (
     <div className="flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
