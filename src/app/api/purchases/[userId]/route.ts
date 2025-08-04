833840bd3cc60a066f9b6ae01209bfd0d13e59a7
ocsoriram <ossoriram@gmail.com>
2025-08-04 18:33:43 +0900

    feat: 書籍購入済確認APIを実装

---
+++ src/app/api/purchases/[userId]/route.ts
@@ -0,0 +1,22 @@
+import prisma from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+// 購入履歴検索API
+export async function GET(
+  request: Request,
+  { params }: { params: { userId: string } }
+) {
+  const parameters = await params;
+  const userId = parameters.userId;
+
+  try {
+    const purchases = await prisma.purchase.findMany({
+      where: { userId: userId },
+    });
+
+    // console.log("log at route.ts", purchases);
+    return NextResponse.json(purchases);
+  } catch (err) {
+    return NextResponse.json(err);
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,24 @@
 // "use client";
 
+import { getServerSession } from "next-auth";
 import Book from "./components/Book";
+import { nextAuthOptions } from "./lib/next-auth/options";
 import { getAllBooks } from "./microcms/client";
-
-// 疑似データ 後で削除
-// const books = [
-//   {
-//     id: 1,
-//     title: "Book 1",
-//     thumbnail: "/thumbnails/discord-clone-udemy.png",
-//     price: 2980,
-//     author: {
-//       id: 1,
-//       name: "Author 1",
-//       description: "Author 1 description",
-//       profile_icon: "https://source.unsplash.com/random/2",
-//     },
-//     content: "Content 1",
-//     created_at: new Date().toString(),
-//     updated_at: new Date().toString(),
-//   },
-//   {
-//     id: 2,
-//     title: "Book 2",
-//     thumbnail: "/thumbnails/notion-udemy.png",
-//     price: 1980,
-//     author: {
-//       id: 2,
-//       name: "Author 2",
-//       description: "Author 2 description",
-//       profile_icon: "https://source.unsplash.com/random/3",
-//     },
-//     content: "Content 2",
-//     created_at: new Date().toString(),
-//     updated_at: new Date().toString(),
-//   },
-//   {
-//     id: 3,
-//     title: "Book 3",
-//     price: 4980,
-//     thumbnail: "/thumbnails/openai-chatapplication-udem.png",
-//     author: {
-//       id: 3,
-//       name: "Author 3",
-//       description: "Author 3 description",
-//       profile_icon: "https://source.unsplash.com/random/4",
-//     },
-//     content: "Content 3",
-//     created_at: new Date().toString(),
-//     updated_at: new Date().toString(),
-//   },
-//   // 他の本のデータ...
-// ];
 
 export default async function Home() {
   const { contents } = await getAllBooks();
-  // console.log("contents: " + JSON.stringify(contents, null, 2));
+  // サーバサイドでセッションから情報を取得する
+  const session = await getServerSession(nextAuthOptions);
+  const user: any = session?.user;
+
+  if (user) {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`
+    );
+    const purchasesData = await response.json();
+    console.log("purchaseData:", purchasesData);
+  }
+
   return (
     <>
       <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
