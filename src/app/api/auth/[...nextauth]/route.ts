21440dc462fafb8b511fe0a17c0fccc15a973246
ocsoriram <ossoriram@gmail.com>
2025-08-06 16:41:09 +0900

    fix:route.tsを修正

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,20 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { getNextAuthOptions } from "@/app/lib/next-auth/options";
 
+export const runtime = "nodejs";
+
 const authOptions: NextAuthOptions = getNextAuthOptions();
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+console.log(
+  "NEXTAUTH_SECRET@runtime:",
+  process.env.NEXTAUTH_SECRET?.slice(0, 6) || "undefined"
+);
+
+console.log(
+  "AUTH_SECRET@runtime:",
+  process.env.AUTH_SECRET?.slice(0, 6) || "undefined"
+);
